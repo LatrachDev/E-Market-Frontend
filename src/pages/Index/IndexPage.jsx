fd0ba1f299dbbf4355e08842dfeb5e46d587fe0c
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-05 11:16:28 +0100

    LUN-4 Create Navbar componentes

--- src/pages/Index/IndexPage.jsx
+++ src/pages/Index/IndexPage.jsx
@@ -1,10 +1,11 @@
+import NavBar from '../../components/Layouts/NavBar';
 import LoginPage from '../Auth/LoginPage';
 
 export default function IndexPage()
 {
     return (
         <>
-            <LoginPage />
+            <NavBar />
         </>
     )
 }
