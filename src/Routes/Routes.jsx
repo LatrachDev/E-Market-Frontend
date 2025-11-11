89c9e1336bf707f3446efac6c71123d8de4eca9d
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-11 14:28:01 +0100

    Admin sidebar component | admin dashboard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import LoginPage from '../pages/Auth/LoginPage';
 import SignupPage from '../pages/Auth/SignupPage';
 import ClientDashboard from "../pages/Client/ClientDashboard";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
 
 export default function RoutesList() {
 
@@ -13,12 +14,17 @@ export default function RoutesList() {
 
     return (
         <Routes>
+
             <Route path="/" element={<IndexPage />} />
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/client" element={<ProtectedRoute><ClientDashboard /></ProtectedRoute>} />
 
+            {/* Admin Routes */}
+            <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+            
+            {/* Error Routes */}
             <Route path={'*'} element={<NotFound />} />
         </Routes>
     )
